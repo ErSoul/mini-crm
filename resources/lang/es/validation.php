867be68d21dc8ad71f1b37791a7b6b26f6c32f1d
David Colmenares <dcolmenares@assanet.com>
2022-06-13 09:02:47 -0400

    Added validation translations.

---
+++ resources/lang/es/validation.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'required' => 'El campo :attribute es obligatorio.',
+    'dimensions' => 'El campo:attribute tiene dimensiones inapropiadas.',
+    'exists' => 'El valor del campo :attribute no se encuentra en nuestros registros.',
+    'numeric' => 'El campo :attribute debe ser un número',
+    'email' => 'El campo :attribute debe ser una dirección de correo válida.',
+    'url' => 'El campo :attribute debe ser una URL válida.',
+    'mimes' => 'El campo :attribute debe ser un archivo de tipo: :values.',
+    'image' => 'El campo :attribute debe ser una imagen.',
+];
